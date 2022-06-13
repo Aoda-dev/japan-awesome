add879ae39b2f24c08f56432517fa07679a2bfd9
Tolegen <hinokamiaoda@gmail.com>
2022-06-13 07:12:35 -0700

    init commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const main = document.getElementById('App')
+
+const tokyo = document.getElementById('Tokyo')
+const kyoto = document.getElementById('Kyoto')
+const osaka = document.getElementById('Osaka')
+const nara = document.getElementById('Nara')
+const glitch = document.getElementById('glitch')
+const backMenu = document.getElementById('backMenu')
+
+const classLists = ['bg-japan', 'bg-tokyo', 'bg-kyoto', 'bg-osaka', 'bg-nara']
+
+const changePage = (pageName) => {
+	glitch.classList.remove('hidden')
+
+	classLists.forEach((cl) => {
+		main.classList.remove(cl)
+	})
+
+	main.classList.add(`bg-${pageName}`)
+
+	setTimeout(() => glitch.classList.add('hidden'), 500)
+}
+
+tokyo.addEventListener('click', () => changePage('tokyo'))
+kyoto.addEventListener('click', () => changePage('kyoto'))
+osaka.addEventListener('click', () => changePage('osaka'))
+nara.addEventListener('click', () => changePage('nara'))
+backMenu.addEventListener('click', () => changePage('japan'))
